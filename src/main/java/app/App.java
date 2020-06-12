f0479abf5e307ed23b33dc7b34d090a7ad622c02
Basia <barbara.anna.adamczyk@outlook.com>
2020-06-12 21:40:34 +0200

    init

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,8 +1,23 @@
 package app;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
 
     public static void main(String[] args) {
+        launch(args);
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/views/frame.fxml"));
+        root.getStylesheets().add(getClass().getResource("/views/style.css").toExternalForm());
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
 }
